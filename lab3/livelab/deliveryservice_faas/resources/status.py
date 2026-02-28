5709ed349c1c22eeefa3df473a97796626a588e4
Indika Kumara <ipkumarawd@yahoo.com>
2026-02-28 23:16:43 +0100

    Fixed an issue with Lab3 Live

--- lab3/livelab/deliveryservice_faas/resources/status.py
+++ lab3/livelab/deliveryservice_faas/resources/status.py
@@ -10,8 +10,9 @@ class Status:
     @staticmethod
     def update(d_id, status_text):
         session = Session()
-        delivery = session.query(DeliveryDAO).filter(DeliveryDAO.id == int(d_id))[0]
+        delivery = session.query(DeliveryDAO).filter(DeliveryDAO.id == int(d_id)).first()
         delivery.status.status_name = status_text
         delivery.status.last_update = datetime.datetime.now()
         session.commit()
+        session.close()
         return jsonify({'message': 'The delivery status was updated'}), 200
