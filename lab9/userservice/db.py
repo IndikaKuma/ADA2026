13889c657777725500cdb3719d3dc87702a1f427
Indika Kumara <ipkumarawd@yahoo.com>
2026-04-16 12:03:50 +0200

    Updated Lab 9 Content

--- lab9/userservice/db.py
+++ lab9/userservice/db.py
@@ -1,8 +1,7 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy_utils import database_exists, create_database
 
 # The database URL is provided as an env. variable
@@ -14,7 +13,6 @@ engine = create_engine(db_url)
 if not database_exists(engine.url):
     create_database(engine.url)
 print(database_exists(engine.url))
-# https://docs.sqlalchemy.org/en/13/orm/session.html
+# https://docs.sqlalchemy.org/en/21/orm/session_basics.html
 Session = sessionmaker(bind=engine)
-# https://docs.sqlalchemy.org/en/13/orm/extensions/declarative/api.html
 Base = declarative_base()
